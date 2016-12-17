1bcfa65c2a3544a4d85fd56f714235c279d2afe7
Andre Reus <andrereus@users.noreply.github.com>
2016-12-17 19:37:47 +0100

    Subtle effects & image compression

--- app/code/js/main.js
+++ app/code/js/main.js
@@ -1,7 +1,7 @@
 var card = $(".card");
 
 $(document).on("mousemove",function(e) {
-    var ax = -($(window).innerWidth()/2-e.pageX)/30;
-    var ay = ($(window).innerHeight()/2-e.pageY)/30;
+    var ax = -($(window).innerWidth()/2-e.pageX)/50;
+    var ay = ($(window).innerHeight()/2-e.pageY)/50;
     card.attr("style", "transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-webkit-transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-moz-transform: rotateY("+ax+"deg) rotateX("+ay+"deg)");
 });
