fd0519591499aa0e5ab14c2180eea6199472351b
Andre Reus <andrereus@users.noreply.github.com>
2016-12-18 14:24:45 +0100

    Optimize page

--- app/code/js/main.js
+++ app/code/js/main.js
@@ -1,13 +1,7 @@
 var card = $(".card");
 
 $(document).on("mousemove",function(e) {
-    var ax = -($(window).innerWidth()/2-e.pageX)/55;
-    var ay = ($(window).innerHeight()/2-e.pageY)/55;
+    var ax = -($(window).innerWidth()/2-e.pageX)/60;
+    var ay = ($(window).innerHeight()/2-e.pageY)/60;
     card.attr("style", "transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-webkit-transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-moz-transform: rotateY("+ax+"deg) rotateX("+ay+"deg)");
-});
-
-$(function() {
-    $("img.lazy").lazyload({
-        threshold: 200
-    });
 });
